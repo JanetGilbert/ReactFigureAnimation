cd1162c3074ea94fde88975f4765eebb8f9acf09
Janet Gilbert <janetcampbell68@hotmail.com>
2026-04-15 21:53:18 -0400

    Fixed issue with face material rendering wrong.

--- src/components/canvas/Tennis.jsx
+++ src/components/canvas/Tennis.jsx
@@ -1,19 +1,32 @@
 'use client'
 
 import { useGLTF, useAnimations } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+// import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { useMemo, useRef, useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 
 export function Tennis(props) {
-  const { scene, animations } = useGLTF('/tennista_palla.glb')
+  const { scene, animations, materials } = useGLTF('/tennista_palla.glb')
   const { actions } = useAnimations(animations, scene)
 
   useEffect(() => {
-    console.log(actions)
     actions.Scene?.play()
   }, [actions])
+
+  useEffect(() => {
+    for (const [key, value] of Object.entries(materials)) {
+      console.log(key + ':')
+      console.log(value)
+      //value.side = THREE.TwoPassDoubleSide
+
+      if (key == 'tennista_occhiblu') {
+        value.depthWrite = true // Bug in model that makes the skin all weird
+      }
+      if (key == 'M_255_255_102_211') {
+        value.visible = false // Hide tennis ball
+      }
+    }
+  }, [materials])
 
   return <primitive object={scene} {...props} />
 }
